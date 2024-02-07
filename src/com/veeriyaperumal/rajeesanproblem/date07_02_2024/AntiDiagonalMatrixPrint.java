25108c679238350fc3bea2794e2c42cceaf895cb
veeriyaperumal7 <148101491+veeriyaperumal7@users.noreply.github.com>
2024-02-07 22:32:16 +0530

    Update code

--- src/com/veeriyaperumal/rajeesanproblem/date07_02_2024/AntiDiagonalMatrixPrint.java
+++ src/com/veeriyaperumal/rajeesanproblem/date07_02_2024/AntiDiagonalMatrixPrint.java
@@ -1,5 +1,8 @@
 package com.veeriyaperumal.rajeesanproblem.date07_02_2024;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Given a N*N square matrix, return an array of its anti-diagonals.
 Look at the example for more details.
@@ -32,24 +35,25 @@ public class AntiDiagonalMatrixPrint {
 	public static void main(String[] args) {
 		int mat[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 		// int mat[][] = { { 1, 2}, { 3, 4}};
-		int row = 0, col = 0;
-		for (int i = 0; i < mat[0].length; i++) {
-			row = 0;
-			col = i;
-			while (col >= 0) {
-				System.out.print(mat[row++][col--] + " ");
-			}
-			System.out.println();
+		int row = mat.length, col = mat[0].length;
+		ArrayList<ArrayList<Integer>> res = new ArrayList();
+
+		for (int i = 0; i < row * 2 - 1; i++) {
+			res.add(new ArrayList<>());
 		}
-		row = 1;
-		col = mat[0].length - 1;
-		while (row < mat.length) {
-			for (int i = 0; row + i < mat.length; i++) {
-				System.out.print(mat[row + i][col - i] + " ");
+
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				res.get(i + j).add(mat[i][j]);
 			}
-			System.out.println();
-			row++;
 		}
+
+		System.out.println("[");
+		for (ArrayList<Integer> list : res) {
+			System.out.println(list);
+		}
+
+		System.out.print("]");
 	}
 
 }
--- src/com/veeriyaperumal/rajeesanproblem/date07_02_2024/FrequencySort.java
+++ src/com/veeriyaperumal/rajeesanproblem/date07_02_2024/FrequencySort.java
@@ -2,14 +2,11 @@ package com.veeriyaperumal.rajeesanproblem.date07_02_2024;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /*
  * Write a program to sort the given array according to frequency of elements.
